6fe9f1a1fa9c938c86601eb3753eb3bb2f324bd4
Od@ <elmorochez22@gmail.com>
2017-09-14 17:10:21 -0400

    porfin se arreglo el menu con submenus

--- app/admin/views/theme/admin.php
+++ app/admin/views/theme/admin.php
@@ -76,96 +76,94 @@
                     <li><a href="<?php echo baseUrl ?>admin/solicitantes">SOLICITANTES</a></li>
                   </ul>
                   <ul class="nav navbar-nav navbar-right">
-                    <li class="dropdown">
-                      <a class="fa fa-cog" href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
-                        
-                        <span class="caret"></span></a>
-                        <ul class="dropdown-menu">
-                          <li><a class="fa fa-sign-out" href="<?php echo baseUrl ?>auth/login/logout">Salir</a></li>
-                        </ul>
-                      </li>
-                    </ul>
-                    </div><!--/.nav-collapse -->
-                  </div>
-                </nav>
+                      <li><a href="#" id="" data-toggle="collapse" data-target="#submenu1" aria-expanded="false"><i class="fa fa-cog"></i> Opciones</a>
+                      <ul class="nav collapse" id="submenu1" role="menu" aria-labelledby="btn-1">
+                        <li><a href="<?php echo baseUrl ?>auth/login/logout">Salir</a></li>
+                      </ul>
+                    </li>
+                  </ul>
+                </div><!--/.nav-collapse -->
               </div>
-              <div class="col-sm-9 col-lg-10">
-                <!-- <img width="100%" height="130px;" src="<?php echo baseUrl ?>/assets/img/banner.jpg" alt=""> -->
-                <br>
-                <?php echo $content ?>
-              </div>
-            </div>
+            </nav>
+            
           </div>
-          <!-- /container -->
-          <!-- MENSAJES FLASH SWEET ALERT 2 -->
-          <?php if (Message::hasMessages()): ?>
-          <?php echo Message::show() ?>
-          <?php endif ?>
-          <?php if (Message::hasQuestion()): ?>
-          <?php echo Message::showQuestion() ?>
-          <?php endif ?>
-          <script type="text/javascript">
-          // get the table element
-          var $table = document.getElementById("tablePagination"),
-          // number of rows per page
-          $n = 5,
-          // number of rows of the table
-          $rowCount = $table.rows.length,
-          // get the first cell's tag name (in the first row)
-          $firstRow = $table.rows[0].firstElementChild.tagName,
-          // boolean var to check if table has a head row
-          $hasHead = ($firstRow === "TH"),
-          // an array to hold each row
-          $tr = [],
-          // loop counters, to start count from rows[1] (2nd row) if the first row has a head tag
-          $i,$ii,$j = ($hasHead)?1:0,
-          // holds the first row if it has a (<TH>) & nothing if (<TD>)
-            $th = ($hasHead?$table.rows[(0)].outerHTML:"");
-            // count the number of pages
-            var $pageCount = Math.ceil($rowCount / $n);
-            // if we had one page only, then we have nothing to do ..
-            if ($pageCount > 1) {
-            // assign each row outHTML (tag name & innerHTML) to the array
-            for ($i = $j,$ii = 0; $i < $rowCount; $i++, $ii++)
-            $tr[$ii] = $table.rows[$i].outerHTML;
-            // create a div block to hold the buttons
-          $table.insertAdjacentHTML("afterend","<div id='buttons'></div");
-          // the first sort, default page is the first one
-          sort(1);
-          }
-          // ($p) is the selected page number. it will be generated when a user clicks a button
-          function sort($p) {
-          /* create ($rows) a variable to hold the group of rows
-          ** to be displayed on the selected page,
-          ** ($s) the start point .. the first row in each page, Do The Math
-          */
-          var $rows = $th,$s = (($n * $p)-$n);
-          for ($i = $s; $i < ($s+$n) && $i < $tr.length; $i++)
-          $rows += $tr[$i];
-          
-          // now the table has a processed group of rows ..
-          $table.innerHTML = $rows;
-          // create the pagination buttons
-          document.getElementById("buttons").innerHTML = pageButtons($pageCount,$p);
-          // CSS Stuff
-          document.getElementById("id"+$p).setAttribute("class","active");
-          }
-          // ($pCount) : number of pages,($cur) : current page, the selected one ..
-          function pageButtons($pCount,$cur) {
-          /* this variables will disable the "Prev" button on 1st page
-          and "next" button on the last one */
-          var $prevDis = ($cur == 1)?"disabled":"",
-          $nextDis = ($cur == $pCount)?"disabled":"",
-          /* this ($buttons) will hold every single button needed
-          ** it will creates each button and sets the onclick attribute
-          ** to the "sort" function with a special ($p) number..
-          */
-          $buttons = "<input type='button' value='&lt;&lt; Prev' onclick='sort("+($cur - 1)+")' "+$prevDis+">";
-          for ($i=1; $i<=$pCount;$i++)
-          $buttons += "<input type='button' id='id"+$i+"'value='"+$i+"' onclick='sort("+$i+")'>";
-          $buttons += "<input type='button' value='Next &gt;&gt;' onclick='sort("+($cur + 1)+")' "+$nextDis+">";
-          return $buttons;
-          }
-          </script>
-        </body>
-      </html>
+          <div class="col-sm-9 col-lg-10">
+            <!-- <img width="100%" height="130px;" src="<?php echo baseUrl ?>/assets/img/banner.jpg" alt=""> -->
+            <br>
+            <?php echo $content ?>
+          </div>
+        </div>
+      </div>
+      <!-- /container -->
+      <!-- MENSAJES FLASH SWEET ALERT 2 -->
+      <?php if (Message::hasMessages()): ?>
+      <?php echo Message::show() ?>
+      <?php endif ?>
+      <?php if (Message::hasQuestion()): ?>
+      <?php echo Message::showQuestion() ?>
+      <?php endif ?>
+      <script type="text/javascript">
+      // get the table element
+      var $table = document.getElementById("tablePagination"),
+      // number of rows per page
+      $n = 5,
+      // number of rows of the table
+      $rowCount = $table.rows.length,
+      // get the first cell's tag name (in the first row)
+      $firstRow = $table.rows[0].firstElementChild.tagName,
+      // boolean var to check if table has a head row
+      $hasHead = ($firstRow === "TH"),
+      // an array to hold each row
+      $tr = [],
+      // loop counters, to start count from rows[1] (2nd row) if the first row has a head tag
+      $i,$ii,$j = ($hasHead)?1:0,
+      // holds the first row if it has a (<TH>) & nothing if (<TD>)
+        $th = ($hasHead?$table.rows[(0)].outerHTML:"");
+        // count the number of pages
+        var $pageCount = Math.ceil($rowCount / $n);
+        // if we had one page only, then we have nothing to do ..
+        if ($pageCount > 1) {
+        // assign each row outHTML (tag name & innerHTML) to the array
+        for ($i = $j,$ii = 0; $i < $rowCount; $i++, $ii++)
+        $tr[$ii] = $table.rows[$i].outerHTML;
+        // create a div block to hold the buttons
+      $table.insertAdjacentHTML("afterend","<div id='buttons'></div");
+      // the first sort, default page is the first one
+      sort(1);
+      }
+      // ($p) is the selected page number. it will be generated when a user clicks a button
+      function sort($p) {
+      /* create ($rows) a variable to hold the group of rows
+      ** to be displayed on the selected page,
+      ** ($s) the start point .. the first row in each page, Do The Math
+      */
+      var $rows = $th,$s = (($n * $p)-$n);
+      for ($i = $s; $i < ($s+$n) && $i < $tr.length; $i++)
+      $rows += $tr[$i];
+      
+      // now the table has a processed group of rows ..
+      $table.innerHTML = $rows;
+      // create the pagination buttons
+      document.getElementById("buttons").innerHTML = pageButtons($pageCount,$p);
+      // CSS Stuff
+      document.getElementById("id"+$p).setAttribute("class","active");
+      }
+      // ($pCount) : number of pages,($cur) : current page, the selected one ..
+      function pageButtons($pCount,$cur) {
+      /* this variables will disable the "Prev" button on 1st page
+      and "next" button on the last one */
+      var $prevDis = ($cur == 1)?"disabled":"",
+      $nextDis = ($cur == $pCount)?"disabled":"",
+      /* this ($buttons) will hold every single button needed
+      ** it will creates each button and sets the onclick attribute
+      ** to the "sort" function with a special ($p) number..
+      */
+      $buttons = "<input type='button' value='&lt;&lt; Prev' onclick='sort("+($cur - 1)+")' "+$prevDis+">";
+      for ($i=1; $i<=$pCount;$i++)
+      $buttons += "<input type='button' id='id"+$i+"'value='"+$i+"' onclick='sort("+$i+")'>";
+      $buttons += "<input type='button' value='Next &gt;&gt;' onclick='sort("+($cur + 1)+")' "+$nextDis+">";
+      return $buttons;
+      }
+      </script>
+    </body>
+  </html>
